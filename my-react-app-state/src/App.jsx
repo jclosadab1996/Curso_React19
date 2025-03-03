96028112e79127533fa7ab3bb5f95e4950037b0a
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-03-03 17:48:18 -0500

    Eventos y esados

--- my-react-app-state/src/App.jsx
+++ my-react-app-state/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import ToggleButton from "./components/ToggleButton";
+import NameForm from "./components/NameForm";
 
 function App() {
   return (
     <>
       <Counter />
+      <ToggleButton />
+      <NameForm />
     </>
   );
 }
