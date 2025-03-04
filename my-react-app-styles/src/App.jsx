8a2fcd79bcf611acf5c951050dce653e0c6c936f
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-03-03 19:35:03 -0500

    Estilos en React

---
+++ my-react-app-styles/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./components/Card//Card";
+import Button from "./components/Button/Button";
+import Text from "./components/Text/Text";
+function App() {
+  return (
+    <>
+      <Card />
+      <Button blue> Hola </Button>
+      <Text />
+    </>
+  );
+}
+
+export default App;
